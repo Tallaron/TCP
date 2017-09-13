c5e75fb63bdbef1e3eafeca01d9278a5c167ae2c
Tallaron <tech@changerslair.de>
2017-09-13 17:18:40 +0200

    - added new constants to settings example

--- src/de/tallaron/tcp/SettingsExample.java
+++ src/de/tallaron/tcp/SettingsExample.java
@@ -15,11 +15,11 @@ abstract public class SettingsExample {
     public static final String REDIRECT_URL = "http://127.0.0.1:27779";
     public static final String SUCCESS_PAGE = "http://127.0.0.1/test/success.php";
     public static final String ERROR_PAGE = "http://127.0.0.1/test/error.php";
-
+    public static final String IRC_SERVER = "irc.chat.twitch.tv";
     
     public static final boolean FORCE_VERIFY = false;
     
-    
     public static final long DEFAULT_HTTP_TIMEOUT = 5000L;
+    public static final int IRC_PORT = 6667;
     
 }
